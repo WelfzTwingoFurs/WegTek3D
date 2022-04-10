8503022135a144aa7eaa318ec786a439f4347ea7
smellyweggy <60985201+WelfzTwingoFurs@users.noreply.github.com>
2022-04-10 15:58:27 -0300

    Add files via upload

---
+++ older_versions/TileMap.gd
@@ -0,0 +1,18 @@
+extends TileMap
+
+func _ready():
+	Worldconfig.TileMap = self
+	if position != Vector2(0,0):
+		print("fix tilemap position")
+		position = Vector2(0,0)
+	
+	visible = false
+
+
+func _process(_delta):
+	if visible == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			visible = false
+	else:
+		if Input.is_action_just_pressed("ui_accept"):
+			visible = true
