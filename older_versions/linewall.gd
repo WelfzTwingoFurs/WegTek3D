b1dbfdc63673d70bb0c6ec4928f9e344b153ac2e
smellyweggy <60985201+WelfzTwingoFurs@users.noreply.github.com>
2022-06-03 11:40:52 -0300

    3/6 new version, moving to older_versions

---
+++ older_versions/linewall.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+onready var line = [to_global($CollisionShape2D.shape.a), to_global($CollisionShape2D.shape.b)]
+
+onready var was_pos = position
+
+func _ready():
+	print(line)
+
+func _process(_delta):
+	if position != was_pos:
+		line = [to_global($CollisionShape2D.shape.a), to_global($CollisionShape2D.shape.b)]
+		was_pos = position
+
+export var height = [2,2,1,1]
