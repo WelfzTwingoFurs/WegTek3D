3d02b0044392d6743b4a46f2d260d6b89430510b
weg <60985201+WelfzTwingoFurs@users.noreply.github.com>
2022-09-28 12:27:32 -0300

    Add files via upload

--- AnimationPlayer _ready.gd
+++ AnimationPlayer _ready.gd
@@ -1,11 +1,4 @@
 extends AnimationPlayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	play("_ready")
