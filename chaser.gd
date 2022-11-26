c2876a487b21cff42079bad6cda69a1a22e2bf2c
weg <60985201+WelfzTwingoFurs@users.noreply.github.com>
2022-11-26 00:07:30 -0300

    Add files via upload

--- chaser.gd
+++ chaser.gd
@@ -5,7 +5,7 @@ const speed = 50
 var distanceXY = Vector2(0,0)
 
 func _physics_process(_delta):
-	motion = lerp(motion, Vector2(speed, speed).rotated(rotation_degrees), 0.1)
+	motion = lerp(motion,  Vector2(speed, 0).rotated((position - Worldconfig.player.position).angle() + PI),  0.1)
 	
 	if motion != Vector2(0,0):
 		$AnimationPlayer.play("walk")
@@ -14,8 +14,8 @@ func _physics_process(_delta):
 		anim = 0
 	
 	distanceXY = position - Worldconfig.player.position
-	rotation_degrees = rad2deg((position - Worldconfig.player.position).angle() + PI/2)
+	rotation_degrees = rad2deg(lerp_angle(deg2rad(rotation_degrees), (position - Worldconfig.player.position).angle() + PI/2, 0.05))
 	
-	#if on_floor == 1 or (col_floors.size() == 0 && positionZ <= 0):
-	#if Worldconfig.player.positionZ > positionZ:
-	#	jump()
+	
+	if Worldconfig.player.positionZ > positionZ:
+		jump()
